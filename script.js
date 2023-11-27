41703017cfdab2783744af4b844abff87ccd9930
nehapokhariya <137785769+nehapokhariya@users.noreply.github.com>
2023-11-27 08:56:54 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,41 @@
+// Add an event listener to checkboxes
+const checkboxes = document.querySelectorAll('.filter-checkbox');
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', filterCards);
+});
+
+function filterCards() {
+    const selectedCategories = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+
+    if (selectedCategories.length === 0) {
+        // If no checkbox is selected, show all cards
+        showAllCards();
+    } else {
+        // Hide all cards
+        hideAllCards();
+
+        // Show only cards that match the selected categories
+        selectedCategories.forEach(category => {
+            const cardsToShow = document.querySelectorAll('.card.' + category);
+            cardsToShow.forEach(card => {
+                card.style.display = 'block';
+            });
+        });
+    }
+}
+
+function hideAllCards() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.style.display = 'none';
+    });
+}
+
+function showAllCards() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.style.display = 'block';
+    });
+}
